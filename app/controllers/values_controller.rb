2565c42702c06c187d3456b7f0856d6fb16aae3f
Aleksey Zolotov <myxrome@gmail.com>
2013-09-19 23:19:02 +0400

    first step of dynamic description form with cocoon.

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -69,6 +69,8 @@ class ValuesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def value_params
-      params[:value].permit(:name, :old_price, :new_price, :discount, :end_date, :url, :category_id)
+      params[:value].permit(:name, :old_price, :new_price, :discount, :end_date, :url, :category_id,
+                            descriptions_attributes: [:id, :description_template_id, :order, :text, :red, :bold,
+                                                      :_destroy])
     end
 end
