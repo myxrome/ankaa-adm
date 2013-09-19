7ab4e8c88e115ee7743d842bcf5c44fa392823c4
Aleksey Zolotov <myxrome@gmail.com>
2013-09-19 17:25:17 +0400

    description model.

---
+++ db/migrate/20130919132451_create_descriptions.rb
@@ -0,0 +1,14 @@
+class CreateDescriptions < ActiveRecord::Migration
+  def change
+    create_table :descriptions do |t|
+      t.integer :description_template_id
+      t.integer :value_id
+      t.integer :order
+      t.text :text
+      t.boolean :red
+      t.boolean :bold
+
+      t.timestamps
+    end
+  end
+end
