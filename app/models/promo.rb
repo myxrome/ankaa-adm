f8bfdd0eb82994e2ddde908222f0f6d10bb6f77c
Aleksey Zolotov <myxrome@gmail.com>
2013-10-01 13:10:47 +0400

    модель для промо и связанные изменения в продукте. related #5

---
+++ app/models/promo.rb
@@ -0,0 +1,12 @@
+class Promo < ActiveRecord::Base
+  belongs_to :value, touch: true, inverse_of: :promos
+
+  Paperclip.interpolates :value_id do |attachment, style|
+    attachment.instance.value_id
+  end
+
+  has_attached_file :image, styles: {xhdpi: ['100%', :png], hdpi: ['75%', :png], mdpi: ['50%', :png], ldpi: ['37%', :png]},
+                    default_style: :xhdpi, url: '/p/:value_id/:style/:hash.:extension',
+                    hash_secret: '933QPmmE2uEvGULxSw4jvWEh'
+
+end
--- app/models/value.rb
+++ app/models/value.rb
@@ -2,5 +2,7 @@ class Value < ActiveRecord::Base
   belongs_to :category, inverse_of: :values
   has_many :descriptions, -> { order 'descriptions.order' }, inverse_of: :value
   accepts_nested_attributes_for :descriptions, :reject_if => :all_blank, :allow_destroy => true
+  has_many :promos, -> { order 'promos.order' }, inverse_of: :value
+  accepts_nested_attributes_for :promos, :reject_if => :all_blank, :allow_destroy => true
 
 end
