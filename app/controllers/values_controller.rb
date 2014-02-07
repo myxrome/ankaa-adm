1d74b2bebde6b8246dca1d030b7de4f166056650
Aleksey Zolotov <myxrome@gmail.com>
2014-02-07 23:18:10 +0400

    добавлено разрешение обновления поля active в контроллере продукта. resolve #32

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -66,7 +66,7 @@ class ValuesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def value_params
-    params[:value].permit(:name, :old_price, :new_price, :discount, :end_date, :url, :category_id,
+    params[:value].permit(:name, :old_price, :new_price, :discount, :end_date, :url, :category_id, :active,
                           descriptions_attributes: [:id, :caption, :order, :text, :red, :bold,
                                                     :_destroy],
                           promos_attributes: [:id, :image, :order, :_destroy])
