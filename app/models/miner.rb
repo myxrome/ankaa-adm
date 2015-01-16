11421d9e78b342e217fd26908ada56b95a5c841e
Aleksey Zolotov <myxrome@gmail.com>
2015-01-16 13:47:52 +0300

    логика майнера выделена в отдельный сервис. ref #75

--- app/models/miner.rb
+++ app/models/miner.rb
@@ -2,42 +2,10 @@ class Miner < ActiveRecord::Base
   belongs_to :category
   has_many :miner_scrapers, inverse_of: :miner, dependent: :destroy
   has_many :scrapers, through: :miner_scrapers
+  before_save :init_name
 
-  @queue = :child
-  def self.perform(id)
-    Miner.find(id).perform
-  end
-
-  attr_accessor :result
-  after_initialize :init_result
-  def init_result
-    self.result = {new: [], updated: [], deleted: [], error: []}
-  end
-
-  def perform
-    unless self.miner_scrapers.empty?
-      source = self.miner_scrapers.map { |miner_scraper|
-        miner_scraper.scraper.perform(self, miner_scraper)
-      }.flatten
-      category.reconcile(source, self) if source
-      MinerMailer.result_email(self, @result).deliver
-    end
-  end
-
-  def on_new(item)
-    self.result[:new] << item unless self.result[:new].include?(item)
-  end
-
-  def on_update(item)
-    self.result[:updated] << item unless self.result[:new].include?(item) && self.result[:updated].include?(item)
-  end
-
-  def on_delete(item)
-    self.result[:deleted] << item
-  end
-
-  def on_error(message)
-    self.result[:error] << message
+  def init_name
+    self.name ||= self.category.name
   end
 
 end
