8674c851ff1977c9833090ed9f9e1206d0c9293f
Aleksey Zolotov <myxrome@gmail.com>
2015-01-17 20:28:29 +0300

    убран тестовый лимит. ref #75

--- app/jobs/data_upload_job.rb
+++ app/jobs/data_upload_job.rb
@@ -2,7 +2,7 @@ class DataUploadJob
 
   @queue = :parent
   def self.perform
-    Miner.all.first(2).map { |miner|
+    Miner.all.map { |miner|
       Resque.enqueue(DataUploadService, miner.id)
     }
   end
